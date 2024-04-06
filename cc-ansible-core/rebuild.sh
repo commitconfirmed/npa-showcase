0aa44622976b63ea01a698cb409826504e3badc8
Andrew Jones <andrew@commitconfirmed.com>
2024-04-06 16:28:36 +1100

    Tidyup and adding clab examples

--- cc-ansible-core/rebuild.sh
+++ cc-ansible-core/rebuild.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
+# Stop, Remove, rebuild & run your Ansible container quickly after making changes
+# Probably better to do in compose but I'm lazy ;)
 sudo docker stop $(sudo docker ps -qaf name=lab_ansible)
-sudo docker rm  $(sudo docker ps -qaf name=lab_ansible)
+sudo docker rm $(sudo docker ps -qaf name=lab_ansible)
 git pull
 sudo docker build -t cc-ansible .
 sudo docker run -d --name lab_ansible cc-ansible
