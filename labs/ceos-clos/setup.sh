c0a89b07a29f7a413393648a7617e55dc394f59f
Andrew Jones <andrew@commitconfirmed.com>
2024-05-18 15:41:49 +1000

    extra sleep

--- labs/ceos-clos/setup.sh
+++ labs/ceos-clos/setup.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 # Build the lab and run ../manage.sh with the relevant playbook / inventory
-sudo containerlab deploy 
+sudo containerlab deploy
+sleep 10 # Give a little bit of time for everything to come up
 bash ../manage.sh -a build -i clos.ini -p pb-import-ssh.yml
