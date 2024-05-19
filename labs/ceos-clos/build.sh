f70dab9709e2582cbcd89f55497215629c0e80e8
Andrew Jones <andrew@commitconfirmed.com>
2024-05-19 23:07:05 +1000

    Migrated to Linux hosts with LACP to save memory

--- labs/ceos-clos/build.sh
+++ labs/ceos-clos/build.sh
@@ -7,3 +7,30 @@ echo "Sleeping for 10 seconds..."
 sleep 10 # Give a little bit of time for everything to come up
 bash ../manage.sh -a build 
 bash ../manage.sh -a deploy -i clos.ini -p pb-import-ssh.yml
+
+# Build host networking
+sudo docker exec -d clab-ceos-clos-host1 ip link add dev bond0 type bond miimon 100 mode 802.3ad
+sudo docker exec -d clab-ceos-clos-host1 ip link set dev eth1 down
+sudo docker exec -d clab-ceos-clos-host1 ip link set dev eth2 down
+sudo docker exec -d clab-ceos-clos-host1 ip link set dev bond0 down
+sudo docker exec -d clab-ceos-clos-host1 ip link set dev eth1 master bond0
+sudo docker exec -d clab-ceos-clos-host1 ip link set dev eth2 master bond0
+sudo docker exec -d clab-ceos-clos-host1 ip link set dev eth1 up
+sudo docker exec -d clab-ceos-clos-host1 ip link set dev eth2 up
+sudo docker exec -d clab-ceos-clos-host1 ip link set dev bond0 up
+sudo docker exec -d clab-ceos-clos-host1 ip link add link bond0 name bond0.100 type vlan id 100
+sudo docker exec -d clab-ceos-clos-host1 ip link set dev bond0.100 up
+sudo docker exec -d clab-ceos-clos-host1 ip addr add 192.168.100.11/24 brd 192.168.100.255 dev bond0.100
+
+sudo docker exec -d clab-ceos-clos-host2 ip link add dev bond0 type bond miimon 100 mode 802.3ad
+sudo docker exec -d clab-ceos-clos-host2 ip link set dev eth1 down
+sudo docker exec -d clab-ceos-clos-host2 ip link set dev eth2 down
+sudo docker exec -d clab-ceos-clos-host2 ip link set dev bond0 down
+sudo docker exec -d clab-ceos-clos-host2 ip link set dev eth1 master bond0
+sudo docker exec -d clab-ceos-clos-host2 ip link set dev eth2 master bond0
+sudo docker exec -d clab-ceos-clos-host2 ip link set dev eth1 up
+sudo docker exec -d clab-ceos-clos-host2 ip link set dev eth2 up
+sudo docker exec -d clab-ceos-clos-host2 ip link set dev bond0 up
+sudo docker exec -d clab-ceos-clos-host2 ip link add link bond0 name bond0.100 type vlan id 100
+sudo docker exec -d clab-ceos-clos-host2 ip link set dev bond0.100 up
+sudo docker exec -d clab-ceos-clos-host2 ip addr add 192.168.100.12/24 brd 192.168.100.255 dev bond0.100
