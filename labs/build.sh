deb789b8277db809f82706c0b2580ab744775b27
Andrew Jones <andrew@commitconfirmed.com>
2024-09-14 23:18:41 +1000

    Script cleanup

---
+++ labs/build.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#
+# Build the relevant lab and run the ansible playbooks
+
+usage="Usage: $(basename "$0") lab-name \n 
+Example to build the ceos-anycast lab - ./$(basename "$0") ceos-anycast"
+
+if [[ -z $1 ]]; then
+  echo $usage
+else
+  # We go into the clab directory so the created clab dir stays in it's relevant lab
+  # (Even though we generally clean it up afterwards)
+  cd $1
+  sudo containerlab deploy 
+  # Give a bit of time for the containers to come up before running ansible playbooks
+  echo "Sleeping for 10 seconds"
+  sleep 10
+  cd ..
+  sudo docker exec -tu ansible -w /app "clab-$1-serv-ansible" ansible-playbook -i inventory/inventory.yml pb-import-ssh.yml
+  sudo docker exec -tu ansible -w /app "clab-$1-serv-ansible" ansible-playbook -i inventory/inventory.yml pb-cfg-lab.yml
+fi
--- labs/ceos-anycast/rebuild.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#
-# Rebuild the lab from scratch after making changes. If you just want to
-# rebuild the ansible container, use the rebuild.sh script in the
-# npa-showcase/cc-ansible-core directory
-
-bash ./cleanup.sh
-bash ./build.sh
